b4206448c7dc4d3069e4aaa2db663c326c32657d
Matěj Šmíd <m@matejsmid.cz>
2018-07-03 17:59:49 +0200

    visualize multiple results in one video

--- mot.py
+++ mot.py
@@ -100,10 +100,13 @@ def generate_colors(count):
     return np.array([cm(1.*i/count, bytes=True)[:3] for i in range(count)]).astype(float)
 
 
-def visualize_mot(video_file, out_video_file, df_mot):
+def visualize_mot(video_file, out_video_file, df_mots, names):
     import cv2
     import tqdm
     # import math
+
+    if names:
+        assert len(names) == len(df_mots)
 
     cap = cv2.VideoCapture(video_file)
     ret, img = cap.read()
@@ -114,19 +117,28 @@ def visualize_mot(video_file, out_video_file, df_mot):
     cap = cv2.VideoCapture(video_file)
 
     writer = cv2.VideoWriter(out_video_file,
-                             cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'),  # ('X', '2', '6', '4'),
+                             cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'),  # ('X', '2', '6', '4'), ('M', 'J', 'P', 'G')
                              cap.get(cv2.CAP_PROP_FPS),  # int(math.ceil(cap.get(cv2.CAP_PROP_FPS))),
                              img_shape[:2][::-1])
 
-    rgb_cycle = generate_colors(df_mot['id'].max())
+    rgb_cycle = generate_colors(max([df['id'].max() for df in df_mots]))
+    markers = [cv2.MARKER_SQUARE, cv2.MARKER_CROSS, cv2.MARKER_DIAMOND, cv2.MARKER_TILTED_CROSS, cv2.MARKER_STAR,
+               cv2.MARKER_TRIANGLE_DOWN, cv2.MARKER_TRIANGLE_UP]
+    assert len(df_mots) <= len(markers)
 
     for frame in tqdm.tqdm(range(1, frame_count + 1)):  # 100)): #
         ret, img = cap.read()
         assert ret
-        for _, row in df_mot[df_mot.frame == frame].iterrows():
-            if not (np.isnan(row.x) or np.isnan(row.y)):
-                cv2.circle(img, (int(row.x), int(row.y)), 8, rgb_cycle[int(row.id - 1)], -1)
-        cv2.putText(img, str(frame), (30, 30), cv2.FONT_HERSHEY_PLAIN, 0.65, (255, 255, 255))
+        for i, df in enumerate(df_mots):
+            for _, row in df[df.frame == frame].iterrows():
+                if not (np.isnan(row.x) or np.isnan(row.y)):
+                    cv2.drawMarker(img, (int(row.x), int(row.y)), rgb_cycle[int(row.id - 1)], markers[i], 10) # , -1)
+        cv2.putText(img, str(frame), (30, 30), cv2.FONT_HERSHEY_PLAIN, 0.8, (255, 255, 255))
+        # show legend
+        if names:
+            for i, (name, marker) in enumerate(zip(names, markers)):
+                cv2.drawMarker(img, (img_shape[1] - 150, 30 + 20 * i - 5), (255, 255, 255), marker, 10)
+                cv2.putText(img, name, (img_shape[1] - 140, 30 + 20 * i), cv2.FONT_HERSHEY_PLAIN, 1, (255, 255, 255))
         writer.write(img)
     writer.release()
 
@@ -150,41 +162,44 @@ if __name__ == '__main__':
     parser.add_argument('--load-tox', type=str, help='load ToxTracker trajectories (e.g., Tracking_0.txt)')
     parser.add_argument('--tox-topleft-xy', nargs='+', type=int, help='position of the arena top left corner, see first tuple in the Arena line in Stats_1.txt')
     parser.add_argument('--load-idtracker', type=str, help='load IdTracker trajectories (e.g., trajectories.txt)')
-    parser.add_argument('--load-mot', type=str, help='load a MOT challenge csv file')
+    parser.add_argument('--load-mot', type=str, nargs='+', help='load a MOT challenge csv file(s)')
     parser.add_argument('--load-gt', type=str, help='load ground truth from a MOT challenge csv file')
     parser.add_argument('--video-in', type=str, help='input video file')
-    parser.add_argument('--video-out', type=str, help='write visualization to a video file')
+    parser.add_argument('--video-out', type=str, help='write visualization(s) to a video file')
     parser.add_argument('--write-mot', type=str, help='write trajectories to a MOT challenge csv file')
     parser.add_argument('--eval', action='store_true', help='evaluate results')
     parser.add_argument('--write-eval', type=str, help='write evaluation results as a csv file')
+    parser.add_argument('--input-names', type=str, nargs='+', help='names of input MOT files')
     args = parser.parse_args()
 
     if args.load_tox:
         assert args.tox_topleft_xy, 'specify position of the arena top left corner using --tox-topleft-xy'
         assert len(args.tox_topleft_xy), 'need to pass exactly two values with --tox-topleft-xy'
-        df = load_toxtrack(args.load_tox, topleft_xy=args.tox_topleft_xy)
+        dfs = [load_toxtrack(args.load_tox, topleft_xy=args.tox_topleft_xy)]
     elif args.load_idtracker:
-        df = load_idtracker(args.load_idtracker)
+        dfs = [load_idtracker(args.load_idtracker)]
     elif args.load_mot:
-        df = load_mot(args.load_mot)
+        dfs = [load_mot(mot) for mot in args.load_mot]
     else:
         assert False, 'no input files specified'
 
     if args.write_mot:
-        save_mot(args.write_mot, df)
-
-    if args.video_out:
-        assert args.video_in
-        visualize_mot(args.video_in, args.video_out, df)
+        assert len(dfs) == 1
+        save_mot(args.write_mot, dfs[0])
 
     if args.eval or args.write_eval:
         assert args.load_gt
+        assert len(dfs) == 1, 'only single input file can be specified for evaluation'
         df_gt = load_mot(args.load_gt)
         print('Evaluating...')
         assert sys.version_info >= (3, 5), 'motmetrics requires Python 3.5'
-        summary = eval_mot(df_gt, df)
+        summary = eval_mot(df_gt, dfs[0])
         import motmetrics as mm
         mh = mm.metrics.create()
         print(mm.io.render_summary(summary, formatters=mh.formatters, namemap=mm.io.motchallenge_metric_names))
         if args.write_eval:
             summary.to_csv(args.write_eval, index=False)
+
+    if args.video_out:
+        assert args.video_in
+        visualize_mot(args.video_in, args.video_out, dfs, args.input_names)
