7255c7574502c3e295dd49f230f306a3a94d6acd
Matěj Šmíd <m@matejsmid.cz>
2019-04-05 10:31:20 +0200

    gt: mot txt files with "type" column
    see /datagrid/ferda/data/groundtruth_visualization/obsolete

--- pkltomot.py
+++ pkltomot.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import fire
+from utils.gt.gt import GT
+import warnings
 
 
 def load_pkl(filename):
@@ -9,28 +11,26 @@ def load_pkl(filename):
     :param filename: ground truth pkl
     :return: mot DataFrame
     """
-    from utils.gt.gt import GT
     gt = GT()
     gt.load(filename)
     gt.set_offset(x=0, y=0, frames=0)
     data = []
     for frame in range(gt.min_frame(), gt.max_frame()):
-        try:
-            for i, yx in enumerate(gt.get_positions(frame), 1):
-                if yx is None:
-                    x = -1
-                    y = -1
-                else:
-                    x = yx[1]
-                    y = yx[0]
-                data.append([frame + 1, i, x, y, -1, -1, 1])
-        except:
-            print(frame)
-    return pd.DataFrame(data, columns=['frame', 'id', 'x', 'y', 'width', 'height', 'confidence'])
+        for i, y_x_type in enumerate(gt.get_positions_and_types(frame), 1):
+            if y_x_type is None:
+                x = -1
+                y = -1
+                gt_type = -1
+            else:
+                y, x, gt_type = y_x_type
+            data.append([frame + 1, i, x, y, -1, -1, 1, gt_type])
+    return pd.DataFrame(data, columns=['frame', 'id', 'x', 'y', 'width', 'height', 'confidence', 'type'])
 
 
 def pkltomot(filename_pkl, filename_mot):
     df = load_pkl(filename_pkl)
+    df = df[df.type != 0]  # gt type == 0 records are removed
+    del df['type']
     df.to_csv(filename_mot, header=False, index=False)
 
 
