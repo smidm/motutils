21ea3b1f82ed9c0baf99b011ff5587b330a4308a
Matěj Šmíd <m@matejsmid.cz>
2021-12-28 16:44:36 +0100

    change legacy project video reading to OpenCV

--- tests/test_oracle_detector_visualize.py
+++ tests/test_oracle_detector_visualize.py
@@ -9,26 +9,18 @@ class OracleDetector(mot_utils.OracleDetectorMixin, mot_utils.Mot):
 
 class OracleDetectorVisualizeTestCase(unittest.TestCase):
     def setUp(self):
-        self.detector = OracleDetector(filename='data/GT/Sowbug3_cut.txt',
+        self.detector = OracleDetector(filename='tests/data/Sowbug3_cut.txt',
                                        bb_size_px=70,
                                        fp_prob=0.05, fn_prob=0.001,
                                        false_detection_jitter_scale=40)
 
     def test_visualize(self):
         import cv2
-        from core.project.project import Project
-        p = Project('test/project/Sowbug3_cut_300_frames')
-        self.detector.set_project_offsets(p)
-
-        try:
-            os.makedirs('test/out/OracleDetectorVisualizeTestCase')
-        except OSError:
-            pass
-
+        cap = cv2.VideoCapture('tests/data/Sowbug3_cut.mp4')
         for frame in range(10):
-            img = p.img_manager.get_whole_img(frame)
+            ret, img = cap.read()
+            assert ret
             for bbox in self.detector.detect(frame):
                 bbox.draw_to_image(img)
                 # bbox.move(-np.array([p.video_crop_model['x1'], p.video_crop_model['y1']])).draw_to_image(img)
-            cv2.imwrite('test/out/OracleDetectorVisualizeTestCase/{:03d}.png'.format(frame), img)
-
+            cv2.imwrite('tests/out/OracleDetectorVisualizeTestCase_{:03d}.png'.format(frame), img)
