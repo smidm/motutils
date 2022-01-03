f85be43d8d5ef985072c0f158b41d6419d234f79
Matěj Šmíd <m@matejsmid.cz>
2022-01-03 06:48:52 +0100

    Bump version: 0.1.0 → 0.1.1

--- motutils/__init__.py
+++ motutils/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Matěj Šmíd"""
 __email__ = "m@matejsmid.cz"
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 
 from .bbox_mot import BboxMot
 from .mot import GtPermutationsMixin, Mot
